7fe469986f98036ce0f906afbd4bf5c6b8877a46
jsawatzky <jsawatzky@outlook.com>
2014-09-10 14:39:49 -0400

    Test

--- src/Unit1/Test.py
+++ src/Unit1/Test.py
@@ -3,4 +3,13 @@ Created on Sep 9, 2014
 
 @author: Jacob
 '''
-print("Hello World")
+
+from turtle import *
+from math import *
+
+t = Pen()
+
+t.seth(0)
+
+for i in range(0, 1024):
+    t.goto(i*20, 20*sin(i))
