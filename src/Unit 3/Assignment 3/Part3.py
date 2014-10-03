41c719a3a1a05f0c0e533f982e97efae89487fb8
Jacob Sawatzky <jsawatzky@outlook.com>
2014-10-02 22:56:09 -0400

    Home

--- src/Unit 3/Assignment 3/Part3.py
+++ src/Unit 3/Assignment 3/Part3.py
@@ -5,6 +5,7 @@ Created on Oct 2, 2014
 '''
 
 from tkinter import *
+from time import sleep
 from utilities import *
 
 #Boolean for update loops
@@ -31,11 +32,36 @@ button = Button(s, text="Done", command = buttonCmd)
 button.configure(width = 10, activebackground = "green")
 s.create_window(710, 760, anchor = NW, window = button)
 
+#Variables
+time = 0 #0-100 = day, 101-200 = night
+season = 0 #0-200 = spring, 201-400 = summer, 401-600 = fall, 601-800 = winter
+weather = 0 #0 = clear, 1 = rain/snow
+
+sun = None
+sunSize = 75
+clouds = []
+stars = []
+asteriods = []
+rain = []
+ground = None
+grass = []
+tree = []
+flowers = []
+
 def update():
-    global s
+    global s, time, season, weather, sun, sunSize, clouds, stars, asteriods, rain, ground, grass, tree, flowers
     #Main animation
+    if time < 101:
+        s.delete(sun)
+        sunY = (0.000625)*((time*8-400)**2) + 100
+        sun = s.create_oval(time*8-sunSize, sunY-sunSize, time*8+sunSize, sunY+sunSize, fill = "yellow")
+    
+    time += 1
+    if time == 201:
+        time = 0
     
     s.update()
+    sleep(0.01)
 
 #Update loop
 while running == True:
