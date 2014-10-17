b260e1d5252ef8acc11faa0998c1f9ba61a85c93
jsawatzky <jsawatzky@outlook.com>
2014-10-17 13:21:59 -0400

    Class

--- src/Unit 3/Assignment 3/PartB/Part3.py
+++ src/Unit 3/Assignment 3/PartB/Part3.py
@@ -6,9 +6,21 @@ Created on Oct 15, 2014
 
 from tkinter import *
 from threading import Timer
+from math import sqrt
 
-WIDTH = 400
+WIDTH = 800
 HEIGHT = 800
+
+s = None
+
+text = None
+
+x = -1
+y = -1
+
+running = False
+
+ripple = None
 
 #Pause
 paused = False
@@ -17,43 +29,62 @@ def unpause():
     global paused
     paused = False
 
-def pause(s, time):
+def pause(time):
     global s, paused
     paused = True
     timer = Timer(time, unpause)
     timer.start()
     while paused == True:
         s.update()
+        
+def click(event):
+    global x, y, running
+    if running == False:
+        x = event.x
+        y = event.y
+        drawRipple()
 
-def drawRipple(s, x, y):
+def drawRipple():
+    global s, WIDTH, HEIGHT, x, y, running, text, ripple
+    
+    running = True
+    s.delete(text)
     
     radius = 0
     
-    ripple = None
+    lUL = sqrt((x**2)+(y**2))
+    lUR = sqrt(((WIDTH-x)**2)+(y**2))
+    lLL = sqrt((x**2)+((HEIGHT-y)**2))
+    lLR = sqrt(((WIDTH-x)**2)+((HEIGHT-y)**2))
     
-    while x-radius > 0 or y-radius > 0 or x+radius < WIDTH or y+radius < HEIGHT:
+    while radius <= lUL or radius <= lUR or radius <= lLL or radius <= lLR:
         
-        s.delete(ripple)
-        
-        s.create_oval(x-radius, y-radius, x+radius, y+radius, fill = None, outline = "black")
+        s.coords(ripple, x-radius, y-radius, x+radius, y+radius)
         s.update()
         
-        radius += 2
+        radius += 4
         
-        pause(0.05)
+        pause(0.02)
+        
+    running = False
 
 def run():
+    global s, x, y, text, ripple
     
     tk = Tk()
-    s = Canvas(tk, width = WIDTH, height = HEIGHT, bg = "blue")
+    s = Canvas(tk, width = WIDTH, height = HEIGHT, bg = "Light Blue")
+    s.bind("<Button-1>", click)
+    ripple = s.create_oval(-5000,-5000, -5000, -5000, fill = None, outline = "blue")
     s.pack()
+    
+    text = s.create_text(400, 400, text = "Click anywhere on the screen", font = "Times 30")
     
     while True:
         
-        x = int(input("X: "))
-        x = int(input("Y: "))
-        
-        drawRipple(s, x, y)
+        try:
+            s.update()
+        except:
+            return
 
 if __name__ == "__main__":
     run()
