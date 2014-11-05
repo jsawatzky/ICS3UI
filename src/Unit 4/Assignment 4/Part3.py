512dad6e6b6c622a42345f20dfc130e9d6f70279
Jacob Sawatzky <jsawatzky@outlook.com>
2014-11-04 22:44:49 -0500

    Home

--- src/Unit 4/Assignment 4/Part3.py
+++ src/Unit 4/Assignment 4/Part3.py
@@ -4,15 +4,15 @@ Created on Oct 29, 2014
 @author: Jacob Sawatzky
 '''
 from tkinter import *
-from math import sqrt
 from utilities import *
 
+#Classes
 class Sun():
     
     def __init__(self, startY, peak):
         self.k = peak
         self.h = 400
-        self.a = sqrt(startY - self.k)/(self.h)
+        self.a = (startY - self.k)/(self.h**2)
         self.object = None
         
     def update(self, time):
@@ -20,15 +20,19 @@ class Sun():
             time -= 1600
         self.x = time
         self.y = self.a*((time-self.h)**2) + self.k
+        print(self.x, self.y)
         
     def draw(self, s):
         if self.object == None:
             self.object = s.create_oval(self.x-75, self.y-75, self.x+75, self.x+75, fill = "yellow", outline = "yellow")
         else:
             s.coords(self.object, self.x-75, self.y-75, self.x+75, self.y+75)
+            
+class Moon():
+    
+    def __init__(self, ):
 
-##Functions go here
-
+##Functions
 def run(tk):
     
     s = Canvas(tk, width =  800, height = 800, background = "blue")
@@ -42,7 +46,7 @@ def run(tk):
         sun.draw(s)
         s.update()
         time += 1
-        pause(0.05, tk)
+        pause(0.01, tk)
     
     clearScreen(tk)
         
