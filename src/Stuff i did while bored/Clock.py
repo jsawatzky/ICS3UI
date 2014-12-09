7200da2cb39a422e7c29cfd71d0e854fbfbd62a8
jsawatzky <jsawatzky@outlook.com>
2014-12-08 21:48:22 -0500

    Home

--- src/Stuff i did while bored/Clock.py
+++ src/Stuff i did while bored/Clock.py
@@ -37,12 +37,15 @@ while True:
     time = str(datetime.now().time())
     
     hour = time[:2]
+    if int(hour) > 12: hour = str(int(hour)-12)
+    elif hour == "0": hour = "12"
     minute = time[3:5]
     second = time[6:8]
+    millis = time[9:]
     
-    secondAngle = radians(6*float(second))
-    minuteAngle = radians(6*float(minute))
-    hourAngle = radians(30*(float(hour)%12))+radians(float(minute)/2)
+    secondAngle = radians(6*int(second))+radians(0.000006*int(millis))
+    minuteAngle = radians(6*int(minute))+radians(0.1*int(second))+radians(0.0000001*int(millis))
+    hourAngle = radians(30*(int(hour)%12))+radians(0.5*int(minute))+radians(0.0083333333*int(second))+radians(0.0000000083333333*int(millis))
     
     secondX = (340*sin(secondAngle))+400
     secondY = -(340*cos(secondAngle))+400
@@ -51,7 +54,7 @@ while True:
     hourX = (240*sin(hourAngle))+400
     hourY = -(240*cos(hourAngle))+400
     
-    s.itemconfig(text, text = time[:8])
+    s.itemconfig(text, text = hour + time[2:8])
     
     s.coords(secondHand, 400, 400, secondX, secondY)
     s.coords(minuteHand, 400, 400, minuteX, minuteY)
